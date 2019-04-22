a82b6fe03881e601937af3448446ec247a162be1
natalizubareva <49855757+natalizubareva@users.noreply.github.com>
2019-04-22 09:15:47 +0300

    Add files via upload

---
+++ my_max.py
@@ -0,0 +1,11 @@
+x=int(input("Enter 1 number"))
+y=int(input("Enter 2 number"))
+if x>y:
+    print (x)
+else:
+    print(y)
+    
+
+       
+
+    
---
+++ table.py
@@ -0,0 +1,11 @@
+value=int (input("Enter element number"))
+if value==3:
+    print("Li")
+elif value==25:
+    print("Mn")
+elif value==80:
+    print("Hg")
+elif value==17:
+    print("Cl")
+elif value!=3 or 25 or 80 or 17:
+    print("IDK")
