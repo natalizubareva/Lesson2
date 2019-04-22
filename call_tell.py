9bf460ea42b3d6cf42b3adf7d9a720a3519c3de5
natalizubareva <49855757+natalizubareva@users.noreply.github.com>
2019-04-22 09:17:42 +0300

    Add files via upload

---
+++ call_tell.py
@@ -0,0 +1,12 @@
+value=int (input("Enter code number"))
+value0=int (input("Enter number of minutes"))
+if value==343:
+    print("cost is",15*value0)
+elif value==381:
+    print("cost is",18*value0)
+elif value==437:
+    print("cost is",13*value0)
+elif value==485:
+    print("cost is",11*value0)
+elif value!=343 or 381 or 437 or 485:
+    print("IDK")
